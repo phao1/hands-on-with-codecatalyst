71dd2ae6d7ff7937aa00b18f7398b9f12b1a2399
Simon Hanmer <simon.hanmer@gmail.com>
2023-01-24 09:46:14 +0000

    add updated test_lambda.py

--- src/unittests/test_lambda.py
+++ src/unittests/test_lambda.py
@@ -1,6 +1,6 @@
 import pytest
 import boto3
-import pprint
+from botocore.exceptions import ClientError
 from lambda_function import *
 
 def test_get_table_name_with_env_var(default_table_name):
@@ -16,13 +16,6 @@ def test_get_table_name_with_no_env_var():
         get_table_name()
 
 
-
-def test_lambda_with_empty_event(good_context, ddb_client, default_table_name, dummy_table, dummy_table_content):
-    """ Check that a standard lambda code returns 200 """
-    status  = lambda_handler({}, good_context)['statusCode']
-    assert status == 200
-    
-    
 def test_ddb_table_exists(ddb_client, default_table_name, dummy_table):
     """ Check that our table exists in the mocked DDB """
     results = ddb_client.list_tables()
@@ -31,13 +24,18 @@ def test_ddb_table_exists(ddb_client, default_table_name, dummy_table):
 
 def test_good_count_invocations(ddb_client, default_table_name, dummy_table, dummy_table_content):
     """ Check that a call to count_invocations in good env returns as expected """
-    
     status, message = count_invocations(ddb_client, default_table_name)
     assert status == 200 and '2 times' in message
     
 
 def test_bad_count_invocations(ddb_client, default_table_name):
-    """ Check that a call to count_invocations in good env returns as expected """
-    
+    """ Check that a call to count_invocations without mocked table """
     status, message = count_invocations(ddb_client, default_table_name)
-    assert status != 200
+    assert status != 200 and 'ResourceNotFound' in message
+
+
+def test_lambda_with_empty_event(good_context, ddb_client, default_table_name, dummy_table, dummy_table_content):
+    """ Check that a standard lambda code returns 200 """
+    results = lambda_handler({}, good_context)
+    assert results['statusCode'] == 200 and '2 times' in results['body']
+    
