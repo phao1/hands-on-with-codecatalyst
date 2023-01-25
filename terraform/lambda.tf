92b651b53dbe8b906129fd23db3562928dd628f0
Simon Hanmer <Simon.Hanmer@ecs.co.uk>
2023-01-25 10:49:45 +0000

    add random prefix to lambda function name

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,8 +4,13 @@ data "archive_file" "lambda_function_zip" {
   output_path = "src/lambda_function.zip"
 }
 
+# Generate a random suffix for our lambda function.
+resource "random_id" "id" {
+	  byte_length = 8
+}
+
 resource "aws_lambda_function" "lambda_function" {
-  function_name                  = "${var.project_name}-lambda-function"
+  function_name                  = "${var.project_name}-lambda-function-${random_id.id.hex}"
   role                           = aws_iam_role.lambda_execution_role.arn
   description                    = "Demo Lambda"
   filename                       = data.archive_file.lambda_function_zip.output_path
